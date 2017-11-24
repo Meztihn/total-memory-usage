43e790f9ee50b912afb66c1c1980ae90cb29ffd9
Alexandr Popov <meztihn@gmail.com>
2017-11-24 13:44:19 +0500

    Add calculating process memory usage on windows

---
+++ src/main/kotlin/com/github/meztihn/memoryusage/Main.kt
@@ -0,0 +1,24 @@
+package com.github.meztihn.memoryusage
+
+import com.github.meztihn.memoryusage.Memory.Unit.*
+
+fun main(args: Array<String>) {
+    args.firstOrNull()?.let { processName ->
+        val memory = processes().filter { it.name.contains(processName, ignoreCase = true) }.map { it.memory }.reduce { total, current -> total + current }
+        println("$processName reserved ${memory.to(GB)} of memory.")
+    } ?: println("Please specify process name.")
+}
+
+private fun processes(): List<Process> = Runtime.getRuntime().exec("tasklist /fo csv /nh").inputStream.use { stream ->
+    stream.bufferedReader().lineSequence().map { line -> parse(line) }.toList()
+}
+
+private val nonDigit = Regex("[^\\d]")
+
+private fun parse(line: String): Process {
+    return line.split(",").map { it.removeSurrounding("\"") }.let { columns ->
+        val name = columns[0]
+        val memory = columns[4].replace(nonDigit, "").toInt()
+        Process(name, Memory(memory, KB))
+    }
+}
